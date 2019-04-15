881689ead21bb9d1a6aa6fce1bfd23fa0d17b89d
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-15 16:15:32 -0400

    add makejdkwithoutconfigure

---
+++ makejdkwithoutconfigure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# duplicate of buildjdk.sh that avoids reconfiguring. Used for making changes to openjdk code.
+
+set -e
+. setdevkitpath.sh
+export FREETYPE_DIR=`pwd`/freetype-2.6.2/build_android-arm
+export CUPS_DIR=`pwd`/cups-2.2.4
+
+# My system's JDK is too old (7.0), so we add an Oracle boot JDK.
+export PATH=`pwd`/jdk-9.0.4/bin:$PATH
+
+cd openjdk
+cd build/android-arm-normal-server-release
+make JOBS=4 images
