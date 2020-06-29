0f3af7f93f9112a72a9b44024186d57865e82d9d
TheTangyLemon <48000527+TheTangyLemon@users.noreply.github.com>
2020-06-29 16:18:12 -0700

    Fix something missed

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -4,4 +4,4 @@ android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
 	--arch=arm64 \
 	--platform=android-21 \
 	--install-dir=`pwd`/android-ndk-r10e/generated-toolchains/android-arm64-toolchain
-cp devkit.info.arm64 android-ndk-r10e/generated-toolchains/android-arm-toolchain/
+cp devkit.info.arm64 android-ndk-r10e/generated-toolchains/android-arm64-toolchain/
