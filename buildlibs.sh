989e26a7db490e3e2a3cbcc04b4a9d8dc0fe889e
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-13 17:14:25 +0700

    Restore

--- buildlibs.sh
+++ buildlibs.sh
@@ -6,19 +6,11 @@ export PATH=$ANDROID_DEVKIT/bin:$PATH
 echo "Building Freetype"
 
 cd freetype-2.6.2
-freetype_build_out=`./configure \
+./configure \
 	--host=aarch64-linux-android \
 	--prefix=`pwd`/build_android-aarch64 \
 	--without-zlib \
 	--with-png=no \
-	--with-harfbuzz=no \
-&& \
-make -j4 && \
-make install` || error_code=$?
-
-if [ "${error_code}" -ne 0 ]; then
-  echo $freetype_build_out
-  exit $error_code
-else
-  echo "Done build freetype"
-fi
+	--with-harfbuzz=no
+make -j4
+make install
