b99b59457f388cb5915477f2a6621db71e8bcd7e
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-14 13:55:37 +0700

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -16,8 +16,10 @@ export LDFLAGS="-L$NDK/platforms/android-21/arch-arm64/usr/lib"
 # Deprecated...
 export AR=$TOOLCHAIN/bin/$TARGET-ar
 export AS=$TOOLCHAIN/bin/$TARGET-as
-export CC=$TOOLCHAIN/bin/$TARGET-clang
-export CXX=$TOOLCHAIN/bin/$TARGET-clang++
+# export CC=$TOOLCHAIN/bin/$TARGET-clang
+# export CXX=$TOOLCHAIN/bin/$TARGET-clang++
+export CC=$TOOLCHAIN/bin/$TARGET-gcc
+export CXX=$TOOLCHAIN/bin/$TARGET-g++
 export LD=$TOOLCHAIN/bin/$TARGET-ld
 export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
 export STRIP=$TOOLCHAIN/bin/$TARGET-strip
