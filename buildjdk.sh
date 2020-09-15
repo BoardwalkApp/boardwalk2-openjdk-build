1041510912960aeda531a72aeeb42bc6d4289a2f
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-16 06:36:31 +0700

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -14,7 +14,7 @@ fi
 sudo apt -y install gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev
 
 # Patch for aarch64
-cp -R -f openjdk-mobile-aarch64-patch/* openjdk/
+# cp -R -f openjdk-mobile-aarch64-patch/* openjdk/
 
 # Remove generated script to regenerate aarch64 build
 # rm openjdk/autoconf/generated-configure.sh
