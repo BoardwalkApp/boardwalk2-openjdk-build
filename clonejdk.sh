1a3e7a0e4ac377c01c953ac127cc7ca980800d35
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-09-19 13:38:58 +0700

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 hg clone http://hg.openjdk.java.net/mobile/jdk9 openjdk
 cd openjdk
 bash ./get_source.sh
@@ -7,10 +7,13 @@ bash ./get_source.sh
 cd hotspot
 hg import ../../termux-openjdk-aarch64-patches/hotspot/*.patch --no-commit
 
+# print debug
+cat src/os_cpu/linux_aarch64/vm/threadLS_linux_aarch64.s.rej
+exit 255
+
 cd ..
 cd jdk
 hg import ../../termux-openjdk-aarch64-patches/jdk/*.patch --no-commit
 
 cd ..
 hg import ../termux-openjdk-aarch64-patches/*.patch --no-commit
-
