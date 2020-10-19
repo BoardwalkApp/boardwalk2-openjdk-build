fb3086a6d0189315c43ef0255c3fe5ebb2b42889
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-19 20:39:52 +0700

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -7,12 +7,12 @@ export CUPS_DIR=`pwd`/cups-2.2.4
 # simplest to force headless:)
 export CPPFLAGS+="-I$FREETYPE_DIR -I$CUPS_DIR -DHEADLESS"
 
-cp -R /usr/include/X11 $ANDROID_INCLUDE/
 # It isn't good, but need make it build anyways
 cp -R $CUPS_DIR/* $ANDROID_INCLUDE/
 
 sudo apt -y install systemtap-sdt-dev gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev libfontconfig1-dev libx11-dev
 
+cp -R /usr/include/X11 $ANDROID_INCLUDE/
 cp -R /usr/include/fontconfig $ANDROID_INCLUDE/
 
 cd openjdk
