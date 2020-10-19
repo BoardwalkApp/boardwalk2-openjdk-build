034fed678693148003cb131d3e547ff8d470e114
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-19 20:26:34 +0700

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e
+set -e
 . setdevkitpath.sh
 export FREETYPE_DIR=`pwd`/freetype-2.6.2/build_android-${TARGET_SHORT}
 export CUPS_DIR=`pwd`/cups-2.2.4
@@ -11,7 +11,7 @@ cp -R /usr/include/X11 $ANDROID_INCLUDE/
 # It isn't good, but need make it build anyways
 cp -R $CUPS_DIR/* $ANDROID_INCLUDE/
 
-sudo apt -y install gcc-multilib g++-multilib systemtap-sdt-devel libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libasound2-dev libelf-dev libfontconfig1-dev
+sudo apt -y install systemtap-sdt-dev gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev libfontconfig1-dev libx11-dev
 
 cp -R /usr/include/fontconfig $ANDROID_INCLUDE/
 
@@ -29,8 +29,7 @@ bash ./configure \
 	--with-freetype-lib=$FREETYPE_DIR/lib \
 	--with-freetype-include=$FREETYPE_DIR/include/freetype2 \
 	--x-includes=$ANDROID_INCLUDE \
-	--x-libraries=/usr/lib
-
+	--x-libraries=/usr/lib || \
 error_code=$?
 if [ "$error_code" -ne 0 ]; then
   echo "\n\nCONFIGURE ERROR $error_code , config.log:"
