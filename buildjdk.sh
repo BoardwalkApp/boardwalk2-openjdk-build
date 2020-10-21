5be58dd8ea23db2220d6090c13c3762285aaeb19
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-21 18:21:01 +0700

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -5,15 +5,17 @@ export FREETYPE_DIR=`pwd`/freetype-2.6.2/build_android-${TARGET_SHORT}
 export CUPS_DIR=`pwd`/cups-2.2.4
 
 # simplest to force headless:)
-export CPPFLAGS+=" -D__linux__=1 -DHEADLESS" # -I$FREETYPE_DIR -I$CUPS_DIR
+export CPPFLAGS+=" -DHEADLESS" # -I$FREETYPE_DIR -I$CUPS_DIR
 
 # It isn't good, but need make it build anyways
 # cp -R $CUPS_DIR/* $ANDROID_INCLUDE/
 
 sudo apt -y install systemtap-sdt-dev gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev libfontconfig1-dev libx11-dev
 
-# cp -R /usr/include/X11 $ANDROID_INCLUDE/
-# cp -R /usr/include/fontconfig $ANDROID_INCLUDE/
+mkdir -p other_include
+
+cp -R /usr/include/X11 other_include/
+cp -R /usr/include/fontconfig other_include/
 
 # Create dummy libraries so we won't have to remove them in OpenJDK makefiles
 mkdir dummy_libs
@@ -33,10 +35,10 @@ bash ./configure \
 	--with-cups-include=$CUPS_DIR \
 	--with-devkit=$ANDROID_DEVKIT \
 	--with-debug-level=release \
-	--with-fontconfig-include=$ANDROID_INCLUDE \
+	--with-fontconfig-include=`pwd`/other_include \
 	--with-freetype-lib=$FREETYPE_DIR/lib \
 	--with-freetype-include=$FREETYPE_DIR/include/freetype2 \
-	--x-includes=$ANDROID_INCLUDE \
+	--x-includes=`pwd`/other_include \
 	--x-libraries=/usr/lib || \
 error_code=$?
 if [ "$error_code" -ne 0 ]; then
