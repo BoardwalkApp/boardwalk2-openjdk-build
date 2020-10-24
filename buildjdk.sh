1f1cd4792afebe8c3aea5ca791d77c431204673a
khanhduytran0 <khanhduytran032@gmail.com>
2020-10-24 15:00:40 +0700

    Update

--- buildjdk.sh
+++ buildjdk.sh
@@ -14,23 +14,16 @@ export LDFLAGS+=" -L`pwd`/dummy_libs -Wl,--warn-unresolved-symbols"
 # cp -R /usr/include/X11 $ANDROID_INCLUDE/
 # cp -R /usr/include/fontconfig $ANDROID_INCLUDE/
 
-ln -s /usr/include/X11 $ANDROID_INCLUDE/
-ln -s /usr/include/fontconfig $ANDROID_INCLUDE/
-
-# TODO remove after use got client to move them permanent
-mkdir -p openjdk/jdk/src/share/native/common/unicode
-mkdir -p openjdk/jdk/src/share/native/sun/font/layout/unicode
-mv openjdk/jdk/src/share/native/sun/font/layout/unicode/* openjdk/jdk/src/share/native/common/unicode/
+ln -s -f /usr/include/X11 $ANDROID_INCLUDE/
+ln -s -f /usr/include/fontconfig $ANDROID_INCLUDE/
 
 # Create dummy libraries so we won't have to remove them in OpenJDK makefiles
-mkdir dummy_libs
+mkdir -p dummy_libs
 ar cru dummy_libs/libpthread.a
 ar cru dummy_libs/libthread_db.a
 
 cd openjdk
 rm -rf build
-
-rm common/autoconf/generated-configure.sh
 
 #	--with-extra-cxxflags="$CXXFLAGS -Dchar16_t=uint16_t -Dchar32_t=uint32_t" \
 #	--with-extra-cflags="$CPPFLAGS" \
