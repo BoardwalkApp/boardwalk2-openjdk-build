f3bd8525eae68e96bf39177f29737d41530e2951
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-25 16:15:43 +0700

    Use correct output j2re-image

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -7,5 +7,5 @@ cd openjdk
 tree
 cd ..
 
-cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-release/jdk jreout
+cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-release/images/j2re-image jreout
 find jreout -name "*.diz" | xargs -- rm
