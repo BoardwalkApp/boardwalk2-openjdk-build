7521bdbdfee7b6b6554785a0b48a56fc121a5173
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-25 16:34:49 +0700

    Update removejdkdebuginfo.sh

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -2,10 +2,5 @@
 set -e
 rm -rf jreout || true
 
-# locate it:)
-cd openjdk
-tree
-cd ..
-
 cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-release/images/j2re-image jreout
 find jreout -name "*.diz" | xargs -- rm
