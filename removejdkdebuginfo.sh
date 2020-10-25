7edc4fd22addcd56081e4d5b25c43900a5919c02
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-10-25 15:06:25 +0700

    Guessing the JDK output dir

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
 rm -rf jreout || true
-cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-release/images/jre jreout
+
+# locate it:)
+ls */*
+
+echo "----- ls more -----"
+ls */*/*
+
+echo "----- ls more -----"
+ls */*/*/*
+
+cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-release/jdk jreout
 find jreout -name "*.diz" | xargs -- rm
