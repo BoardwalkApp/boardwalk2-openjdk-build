3b36ce19cf41a0d8465a172dc9677bfa3f358155
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-11-01 04:38:22 +0700

    Update buildlibs.sh

--- buildlibs.sh
+++ buildlibs.sh
@@ -11,7 +11,14 @@ cd freetype-2.6.2
 	--prefix=`pwd`/build_android-${TARGET_SHORT} \
 	--without-zlib \
 	--with-png=no \
-	--with-harfbuzz=no
+	--with-harfbuzz=no || \
+error_code=$?
+if [ "$error_code" -ne 0 ]; then
+  echo "\n\nCONFIGURE ERROR $error_code , config.log:"
+  cat config.log
+  exit $error_code
+fi
+
 make -j4 | \
 sudo apt -y install systemtap-sdt-dev gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev libfontconfig1-dev libx11-dev
 
