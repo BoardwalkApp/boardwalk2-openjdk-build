70d8b94e0370918db3cdad79ae4a792f98c13ddd
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-11-01 09:05:54 +0700

    Speed up OpenJDK clone time

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/PojavLauncherTeam/openjdk-aarch64-jdk8u openjdk
+git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch64-jdk8u openjdk
