9fe3099f5b47649b49a505e2989126dab42513cd
khanhduytran0 <khanhduytran032@gmail.com>
2020-11-04 12:10:35 +0700

    Allow pack debugging .diz files

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -4,4 +4,4 @@ rm -rf jreout || true
 
 cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-${JDK_DEBUG_LEVEL}/images/j2re-image jreout
 mv jreout/lib/${TARGET_JDK}/libfreetype.so.6 jreout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
-find jreout -name "*.diz" | xargs -- rm
+# find jreout -name "*.diz" | xargs -- rm
