092b2774601a4c1880cacd5a338e4a64c8421e02
Tran Khanh Duy <40482367+khanhduytran0@users.noreply.github.com>
2020-11-09 21:16:07 +0700

    Remove libjvm.diz

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -5,6 +5,8 @@ rm -rf jreout || true
 cp -r openjdk/build/linux-${TARGET_JDK}-normal-server-${JDK_DEBUG_LEVEL}/images/j2re-image jreout
 mv jreout/lib/${TARGET_JDK}/libfreetype.so.6 jreout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
 
-mv jreout/lib/${TARGET_JDK}/libfontmanager.diz jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep
-find jreout -name "*.diz" | xargs -- rm
-mv jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep jreout/lib/${TARGET_JDK}/libfontmanager.diz
+# mv jreout/lib/${TARGET_JDK}/libfontmanager.diz jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep
+# find jreout -name "*.diz" | xargs -- rm
+# mv jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep jreout/lib/${TARGET_JDK}/libfontmanager.diz
+
+rm jreout/lib/${TARGET_JDK}/server/libjvm.diz
