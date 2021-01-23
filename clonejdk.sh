9bc8982e9903790d95ba367c219b2623bc0087e4
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-01-23 09:45:16 +0300

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 if [ "$TARGET_JDK" == "arm" ]
+then
 git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch64-jdk8u openjdk
 else
 git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u openjdk
