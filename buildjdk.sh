f79475d77032f944c559304a146d107a6f4301a5
ArtDev <45949002+artdeell@users.noreply.github.com>
2021-01-23 19:12:47 +0300

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$TARGET_JDK" == "arm" ]
 then
   export TARGET_JDK=aarch32
-  export TARGET_PHYS=aarch32-linux-gnueabi
+  export TARGET_PHYS=aarch32-linux-androideabi
 else
   export TARGET_PHYS=$TARGET
 fi
