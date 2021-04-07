1de27a9617cb4fb050410176095031f324231ff9
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-04-07 16:01:08 +0700

    Use termux-elf-cleaner

--- tarjdk.sh
+++ tarjdk.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
+
+# cleanup ELF stuff
+git clone https://github.com/termux/termux-elf-cleaner
+cd termux-elf-cleaner
+make CFLAGS=__ANDROID_API__=24 termux-elf-cleaner
+chmod +x termux-elf-cleaner
+cd ..
+
 mv jre_override/lib/* jreout/lib/
+
+find jreout -name "*.so" | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
+find jdkout -name "*.so" | xargs -- ./termux-elf-cleaner/termux-elf-cleaner
+
 cd jreout
 tar cJf ../jre8-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
+
 cd ../jdkout
 tar cJf ../jdk8-${TARGET_SHORT}-`date +%Y%m%d`-${JDK_DEBUG_LEVEL}.tar.xz .
 
