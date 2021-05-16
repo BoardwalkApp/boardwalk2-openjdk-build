c4ef6392f399e0791d4c2e75b575e2b049835b2f
pojavlauncher <khanhduytran0@users.noreply.github.com>
2021-05-16 17:22:46 +0700

    Fixed building on macOS Big Sur

--- buildlibs.sh
+++ buildlibs.sh
@@ -6,19 +6,23 @@ cd freetype-$BUILD_FREETYPE_VERSION
 echo "Building Freetype"
 
 if [ "$BUILD_IOS" == "1" ]; then
+  LDFLAGS=-"arch arm64 -isysroot $thesysroot -miphoneos-version-min=12.0"
+
   export CC=$thecc
   export CXX=$thecxx
   ./configure \
     --host=$TARGET \
-    --prefix=`pwd`/build_android-${TARGET_SHORT} \
+    --prefix=${PWD}/build_android-${TARGET_SHORT} \
+    --enable-shared=no --enable-static=yes \
     --without-zlib \
     --with-brotli=no \
     --with-png=no \
     --with-harfbuzz=no \
     "CFLAGS=-arch arm64 -pipe -std=c99 -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=12.0 -I$thesysroot/usr/include/libxml2/ -isysroot $thesysroot" \
     AR=/usr/bin/ar \
-    "LDFLAGS=-arch arm64 -isysroot $thesysroot -miphoneos-version-min=12.0" \
+    "LDFLAGS=$LDFLAGS" \
     || error_code=$?
+namefreetype=build_android-${TARGET_SHORT}/lib/libfreetype
 else
   export PATH=$TOOLCHAIN/bin:$PATH
   ./configure \
@@ -38,3 +42,6 @@ fi
 CFLAGS=-fno-rtti CXXFLAGS=-fno-rtti make -j4
 make install
 
+if [ -f "${namefreetype}.a" ]; then
+  clang -fPIC -shared $LDFLAGS -lbz2 -Wl,-all_load ${namefreetype}.a -o ${namefreetype}.dylib
+fi
