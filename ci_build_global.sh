f9594f38ff798dd6ea0dbf997eebbdd0de872d18
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-05-18 11:54:43 +0700

    Set debug level to slowdebug for iOS port

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
+. setdevkitpath.sh
 
 export JDK_DEBUG_LEVEL=release
 
 if [ "$BUILD_IOS" != "1" ]; then
   sudo apt update
-fi
-. setdevkitpath.sh
 
-if [ "$BUILD_IOS" == "1" ]; then
-  chmod +x ios-arm64-clang
-  chmod +x ios-arm64-clang++
-else
   wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
   ./extractndk.sh
   ./maketoolchain.sh
+else
+  # OpenJDK 8 iOS port is still in unusable state, so we need build in debug mode
+  export JDK_DEBUG_LEVEL=slowdebug
+
+  chmod +x ios-arm64-clang
+  chmod +x ios-arm64-clang++
+  chmod +x macos-host-cc
 fi
 
 # Some modifies to NDK to fix
