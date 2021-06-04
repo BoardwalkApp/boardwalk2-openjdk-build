a17db03469ed1c49d8c12683ed351cfa300dfbba
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-06-04 17:59:51 +0700

    First attempt to build jdk16 for android

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-if [ "$TARGET_JDK" == "arm" ]
-then
-git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u openjdk
-else
-git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch64-jdk8u openjdk
-fi
+
+git clone --depth 1 --single-branch jdk16.0.1 https://github.com/openjdk/jdk16u openjdk
