929aa7f50ce3ea0618909a89c31e2ea59efe9f3a
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-06-04 18:21:56 +0700

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --depth 1 --single-branch jdk16.0.1 https://github.com/openjdk/jdk16u openjdk
+git clone --depth 1 --branch jdk16.0.1 https://github.com/openjdk/jdk16u openjdk
