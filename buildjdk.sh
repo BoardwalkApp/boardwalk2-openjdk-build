5c5f307093db140b728b7e1da94f5b31de4a7d54
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-06-05 13:24:28 +0700

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -83,7 +83,7 @@ if [ "$error_code" -ne 0 ]; then
   exit $error_code
 fi
 
-cd build/${JVM_PLATFORM}-${TARGET_JDK}-normal-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}
+cd build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}
 make JOBS=4 images || \
 error_code=$?
 if [ "$error_code" -ne 0 ]; then
