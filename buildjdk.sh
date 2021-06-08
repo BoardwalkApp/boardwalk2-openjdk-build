5ac13971e41ecef1560106d7b9180330e2a6fb04
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-06-08 09:13:48 +0700

    Create dummy librt

--- buildjdk.sh
+++ buildjdk.sh
@@ -31,13 +31,14 @@ if [ "$BUILD_IOS" != "1" ]; then
   ln -s -f /usr/include/fontconfig $ANDROID_INCLUDE/
   AUTOCONF_x11arg="--x-includes=$ANDROID_INCLUDE/X11"
 
-  export LDFLAGS+=" -L`pwd`/dummy_libs"
+  export LDFLAGS+=" -L$PWD/dummy_libs"
 
   sudo apt -y install systemtap-sdt-dev gcc-multilib g++-multilib libxtst-dev libasound2-dev libelf-dev libfontconfig1-dev libx11-dev libxext-dev libxrandr-dev libxrender-dev libxtst-dev libxt-dev
 
 # Create dummy libraries so we won't have to remove them in OpenJDK makefiles
   mkdir -p dummy_libs
   ar cru dummy_libs/libpthread.a
+  ar cru dummy_libs/librt.a
   ar cru dummy_libs/libthread_db.a
 else
   ln -s -f /opt/X11/include/X11 $ANDROID_INCLUDE/
