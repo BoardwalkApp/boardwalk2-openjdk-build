e7b75eedc0fd10021c769c08148ac0c7da061d74
pojavlauncher <khanhduytran0@users.noreply.github.com>
2021-06-30 13:39:09 +0700

    zulu port test

--- buildjdk.sh
+++ buildjdk.sh
@@ -40,7 +40,7 @@ if [ "$BUILD_IOS" != "1" ]; then
   ar cru dummy_libs/libthread_db.a
 else
   ln -s -f /opt/X11/include/X11 $ANDROID_INCLUDE/
-  platform_args=--with-toolchain-type=clang
+  platform_args="--with-toolchain-type=clang"
   AUTOCONF_x11arg="--with-x=/opt/X11/include/X11 --disable-precompiled-headers --prefix=/usr/lib"
   sameflags="-arch arm64 -isysroot $thesysroot -miphoneos-version-min=12.0 -DHEADLESS=1 -I$PWD/ios-missing-include -Wno-implicit-function-declaration"
   export CFLAGS+=" $sameflags"
--- clonejdk.sh
+++ clonejdk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-if [ "$TARGET_JDK" == "arm" ]
-then
+if [ "$TARGET_JDK" == "arm" ]; then
 git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u openjdk
+elif [ "$BUILD_IOS" == "1" ]; then
+git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
-git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch64-jdk8u openjdk
+git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 fi
