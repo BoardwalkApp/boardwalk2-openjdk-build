315532cdf2debd0274de6433a51876ce6b85ac63
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-07-12 13:29:21 +0700

    Fix libfreetype RPATH

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -13,10 +13,10 @@ mkdir dizout
 cp -r $imagespath/j2re-image jreout
 cp -r $imagespath/j2sdk-image jdkout
 
-mv jdkout/lib/${TARGET_JDK}/libfreetype.so.6 jdkout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
-mv jdkout/lib/${TARGET_JDK}/libfreetype.dylib.6 jdkout/lib/${TARGET_JDK}/libfreetype.dylib || echo "Move exit $?"
+mv jdkout/jre/lib/${TARGET_JDK}/libfreetype.so.6 jdkout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
+mv jdkout/jre/lib/libfreetype.dylib.6 jdkout/jre/lib/libfreetype.dylib || echo "Move exit $?"
 mv jreout/lib/${TARGET_JDK}/libfreetype.so.6 jreout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
-mv jreout/lib/${TARGET_JDK}/libfreetype.dylib.6 jreout/lib/${TARGET_JDK}/libfreetype.dylib || echo "Move exit $?"
+mv jreout/lib/libfreetype.dylib.6 jreout/lib/libfreetype.dylib || echo "Move exit $?"
 
 # mv jreout/lib/${TARGET_JDK}/libfontmanager.diz jreout/lib/${TARGET_JDK}/libfontmanager.diz.keep
 # find jreout -name "*.diz" | xargs -- rm
@@ -26,6 +26,11 @@ find jreout -name "*.diz" -delete
 find jdkout -name "*.diz" -exec mv {} dizout/ \;
 
 if [ "$BUILD_IOS" == "1" ]; then
+  install_name_tool -id @rpath/libfreetype.dylib jdkout/jre/lib/libfreetype.dylib
+  install_name_tool -id @rpath/libfreetype.dylib jreout/lib/libfreetype.dylib
+  install_name_tool -change build_android-arm64/lib/libfreetype.dylib @rpath/libfreetype.dylib jdkout/jre/lib/libfontmanager.dylib
+  install_name_tool -change build_android-arm64/lib/libfreetype.dylib @rpath/libfreetype.dylib jreout/lib/libfontmanager.dylib
+
   JAVA_HOME=/usr/lib/jvm/java-8-openjdk
   for dafile in $(find j*out -name "*.dylib"); do
     install_name_tool -add_rpath $JAVA_HOME/lib/server -add_rpath $JAVA_HOME/lib/jli \
