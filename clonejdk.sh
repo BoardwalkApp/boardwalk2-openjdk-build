14f2981ab32916e7349c04a5cf4e0893c3fa62c7
Duy Tran Khanh <khanhduytran0@users.noreply.github.com>
2023-02-19 17:53:35 +0700

    Build for aarch32 using its repo

--- clonejdk.sh
+++ clonejdk.sh
@@ -5,5 +5,6 @@ git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u o
 elif [ "$BUILD_IOS" == "1" ]; then
 git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
-git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
+# Use aarch32 repo because it also has aarch64
+git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 fi
