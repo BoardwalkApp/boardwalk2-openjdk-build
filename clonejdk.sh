4a07e97efe3859d6677e4c836c99b326f2850e05
ArtDev <45949002+artdeell@users.noreply.github.com>
2023-02-24 15:22:35 +0300

    Use aarch32 port for aarch32 and base jdk8u otherwise

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 if [ "$TARGET_JDK" == "arm" ]; then
-git clone --depth 1 https://github.com/PojavLauncherTeam/openjdk-aarch32-jdk8u openjdk
+git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [ "$BUILD_IOS" == "1" ]; then
 git clone --depth 1 --branch ios https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u openjdk
 else
 # Use aarch32 repo because it also has aarch64
-git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
+
+git clone --depth 1 https://github.com/openjdk/jdk8u openjdk
 fi
