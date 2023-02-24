1a5ab53056a6943a68f7ef2e9eb804c88c76f493
artdeell <ultramaksim@gmail.com>
2023-02-24 16:26:18 +0300

    Fix missing semicolon

--- buildjdk.sh
+++ buildjdk.sh
@@ -64,7 +64,7 @@ cd openjdk
 if [ "$BUILD_IOS" != "1" ]; then
   git reset --hard
   git apply --reject --whitespace=fix ../patches/jdk8u_android.diff || echo "git apply failed (universal patch set)"
-  if [ "$TARGET_JDK" == "arm" ] then
+  if [ "$TARGET_JDK" == "arm" ]; then
     #TODO: make separate aarch32 patch set
   else
     git apply --reject --whitespace=fix ../patches/jdk8u_android_main.diff || echo "git apply failed (main non-universal patch set)"
