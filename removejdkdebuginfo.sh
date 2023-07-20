c92ce2a7376317f7795980fd94352c02b6d0a359
ArtDev <45949002+artdeell@users.noreply.github.com>
2023-07-20 16:07:38 +0300

    Fix libfreetype paths for x86

--- removejdkdebuginfo.sh
+++ removejdkdebuginfo.sh
@@ -13,6 +13,10 @@ mkdir dizout
 cp -r $imagespath/j2re-image jreout
 cp -r $imagespath/j2sdk-image jdkout
 
+if [ "$TARGET_JDK" == "x86" ]; then
+  export TARGET_JDK=i386
+fi
+
 mv jdkout/jre/lib/${TARGET_JDK}/libfreetype.so.6 jdkout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
 mv jdkout/jre/lib/libfreetype.dylib.6 jdkout/jre/lib/libfreetype.dylib || echo "Move exit $?"
 mv jreout/lib/${TARGET_JDK}/libfreetype.so.6 jreout/lib/${TARGET_JDK}/libfreetype.so || echo "Move exit $?"
